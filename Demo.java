5fb68f82e4bd626b0eb6950e65737b6e51592965
SWATHIKARTHIKEYAN208 <shreeswathi1996@gmail.com>
2023-10-03 10:31:10 +0530

    changed

--- Demo.java
+++ Demo.java
@@ -3,10 +3,10 @@ package exception;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
+		System.out.println("hello");
 		int a=10;
 		int b=0;
 		System.out.println(a/b);
-		System.out.println("bye");
+		System.out.println("Okie");
 	}
 }
--- Login.java
+++ Login.java
@@ -3,7 +3,7 @@ package exception;
 public class Login extends Exception {
 	public void checkPassword(String a)throws Exception {
 		if(a.length()>8) {
-		System.out.println("Login successfully");
+		System.out.println("Login Done");
 	}
 		else {
 			throw new Exception("Password Exception");
