78df1b6124b91c558635146f0cfed4fcab59d95e
SWATHIKARTHIKEYAN208 <146800574+SWATHIKARTHIKEYAN208@users.noreply.github.com>
2023-10-03 11:26:29 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -5,8 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		int a=10;
-		int b=0;
+		int b=2;
 		System.out.println(a/b);
+		System.out.println(a+b);
 		System.out.println("Okie");
 	}
 }
